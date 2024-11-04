f3a702dc342520d35d9deb5e0a0e2e7b9b8ec0d1
Noa-ST <kietnhonmy0000@gmail.com>
2024-11-04 20:34:01 +0700

    Passive Items and Inventory

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,22 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
     public List<WeaponController> weaponSlots = new List<WeaponController>(6);
     public int[] weaponLevels = new int[6];
+    public List<Image> weaponUISlots = new List<Image>(6);
     public List<PassiveItems> passiveItemSlots = new List<PassiveItems>(6);
     public int[] passiveItemLevels = new int[6];
+    public List<Image> passiveItemUISlots = new List<Image>(6);
 
     public void AddWeapon(int slotIndex, WeaponController weapon)
     {
         weaponSlots[slotIndex] = weapon;
+        weaponLevels[slotIndex] = weapon.weaponData.Level;
+        weaponUISlots[slotIndex].enabled = true;
+        weaponUISlots[slotIndex].sprite = weapon.weaponData.Icon;
     }
 
     public void AddPassiveItem(int slotIndex, PassiveItems passiveItems)
     {
         passiveItemSlots[slotIndex] = passiveItems;
+        passiveItemLevels[slotIndex] = passiveItems.passiveitemData.Level;
+        passiveItemUISlots[slotIndex].enabled = true;
+        passiveItemUISlots[slotIndex].sprite = passiveItems.passiveitemData.Icon;
     }
 
     public void LevelUpWeapon(int slotIndex)
